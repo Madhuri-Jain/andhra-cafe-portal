9fa0620b4f1a1a4f521ed952294b82df8a1a4339
Madhuri Jain <madhurijain@Madhuris-MacBook-Air.local>
2025-05-12 10:38:10 +0100

    react vite js tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-100">Welcome to Andhra Cafe</h1>
+      <Home />
+    </>
+  )
+}
+
+export default App
